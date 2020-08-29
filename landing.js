f0cdcb5048d51e32e2ddf5d8989657dc7ee914be
Bui Khac Nam <buikhacnam11@gmail.com>
2020-08-29 12:31:35 +0700

    sds

--- landing.js
+++ landing.js
@@ -41,9 +41,10 @@ function setBgGreet() {
 
 	if (hour < 12) {
 		document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1528163073829-df0b94e210a8?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=83b5db9f6094f5de650156ea3ddc9f0e&auto=format&fit=crop&w=1920') ";
-		greeting.textContent = 'Good Moring';
+		greeting.textContent = 'Good Morning';
 	}else if (hour < 18) {
-		document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1531387079845-b3bcdd83a14c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e8e37aac5488d819ce9ee1dd83988de3&auto=format&fit=crop&w=1920')  ";
+		document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1528163073829-df0b94e210a8?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=83b5db9f6094f5de650156ea3ddc9f0e&auto=format&fit=crop&w=1920') ";
+		greeting.textContent = 'Good Morning';
 		greeting.textContent = 'Good Afternoon';
 	} else {
 		document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1502657877623-f66bf489d236?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c4a086173e78551f89a3e22d03a8053f&auto=format&fit=crop&w=1920')";
@@ -121,7 +122,7 @@ function getResult(query) {
 function displayResult(weather) {
 	console.log(weather);
 	let city = document.querySelector('.location .city h1');
-	city.innerText = `${weather.name}, ${weather.sys.country}`;
+	city.innerText = `${weather.name},${weather.sys.country}`;
 
 	let temp = document.querySelector('.current .temp h1');
 	temp.innerText = `${weather.main.temp}°C`;
